96c5e7a282f4dc15c3a588b74aad0d81d6e22a9d
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-04-10 18:51:46 -0600

    cambio

--- app/src/main/java/com/example/medilife/MainActivity.kt
+++ app/src/main/java/com/example/medilife/MainActivity.kt
@@ -7,6 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var hola:String;
+        var hola:String="hi";
+
+        if(hola=="hi"){
+
+        }
+        else{
+
+        }
     }
 }
