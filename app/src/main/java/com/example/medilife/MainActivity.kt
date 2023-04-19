ad2fcf7f77511852684e40f38085df1efda3e914
Fran <20220326@ricaldone.edu.sv>
2023-04-19 09:54:28 -0600

    Degradado

--- app/src/main/java/com/example/medilife/MainActivity.kt
+++ app/src/main/java/com/example/medilife/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //Hola porque no sirve esto
     }
 }
