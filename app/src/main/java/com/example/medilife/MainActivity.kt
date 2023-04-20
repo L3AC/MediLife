8aaad87ca60b3624230ec7ea753b9c96e9ccc1b0
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-04-19 20:53:41 -0600

    cambio

--- app/src/main/java/com/example/medilife/MainActivity.kt
+++ app/src/main/java/com/example/medilife/MainActivity.kt
@@ -5,26 +5,126 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 
-lateinit var editUsu:EditText
-lateinit var editContra:EditText
-lateinit var bIngresar:Button
+lateinit var editUsu: EditText
+lateinit var editContra: EditText
+lateinit var bIngresar: Button
+
 class MainActivity : AppCompatActivity() {
-
+    private var conx = Conx()
+    var idTipo: Int = 0
+    var idUs: Int = 0
+    var idCuenta: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editUsu=findViewById(R.id.txtUsuario)
-        editContra=findViewById(R.id.txtContra)
-        bIngresar=findViewById(R.id.btnIngresar)
+        editUsu = findViewById(R.id.txtUsuario)
+        editContra = findViewById(R.id.txtContra)
+        bIngresar = findViewById(R.id.btnIngresar)
 
-        bIngresar.setOnClickListener{
+        bIngresar.setOnClickListener {
 
         }
 
     }
-    fun Ingresar(){
-        //val intent = Intent(this, ::class.java)
+
+    fun VerifTipo() {
+        val cadena: String = "select * from tbUsuarios where usuario='poji' " +
+                "COLLATE SQL_Latin1_General_CP1_CS_AS and contra='poji' COLLATE SQL_Latin1_General_CP1_CS_AS"
+        val st: ResultSet
+        val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+
+        ps.setString(1, editUsu.text.toString())
+        ps.setString(2, editContra.text.toString())
+        st = ps.executeQuery()
+        st.next()
+
+        val found = st.row
+        if (found == 1) {
+            idTipo = st.getInt("idTipo")//Si es secretaria, doctor o cliente
+            idUs = st.getInt("idUsuario")
+            //Toast.makeText(applicationContext,"Acceso completado", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+        }
+        conx.dbConn()!!.close()
     }
+
+    fun VerifDatos() {
+        val scndAct = Intent(this, MainInside::class.java)
+        //DOCTORES
+        if (idTipo == 1) {
+            val cadena: String = "select idDoctor from tbDoctores inner join tbUsuarios " +
+                    "on tbDoctores.idUsuario=tbUsuarios.idUsuario\n" +
+                    "where tbDoctores.idUsuario=?;"
+            val st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+
+            ps.setString(1, idUs.toString())
+            st = ps.executeQuery()
+            st.next()
+
+            val found = st.row
+            if (found == 1) {
+                idCuenta = st.getInt("idDoctor")//Obtener id
+                Toast.makeText(applicationContext, "Acceso completado", Toast.LENGTH_SHORT).show()
+                scndAct.putExtra("idCuenta", idCuenta)
+                startActivity(scndAct)
+            } else {
+                Toast.makeText(applicationContext, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+            conx.dbConn()!!.close()
+        }
+        //SECRETARIAS
+        if (idTipo == 2) {
+            val cadena: String = "select idSecretaria from tbSecretarias inner join " +
+                    "tbUsuarios on tbSecretarias.idUsuario=tbUsuarios.idUsuario\n" +
+                    "where tbSecretarias.idUsuario=?;"
+            val st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+
+            ps.setString(1, idUs.toString())
+            st = ps.executeQuery()
+            st.next()
+
+            val found = st.row
+            if (found == 1) {
+                idCuenta = st.getInt("idSecretaria")
+                scndAct.putExtra("idCuenta", idCuenta)
+                startActivity(scndAct)
+                Toast.makeText(applicationContext, "Acceso completado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+            conx.dbConn()!!.close()
+        }
+        //CLIENTES
+        if (idTipo == 3) {
+            val cadena: String = "select idCliente from tbCliente inner join " +
+                    "tbUsuarios on tbSecretarias.idUsuario=tbUsuarios.idUsuario\n" +
+                    "where tbSecretarias.idUsuario=?;"
+            val st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+
+            ps.setString(1, idUs.toString())
+            st = ps.executeQuery()
+            st.next()
+
+            val found = st.row
+            if (found == 1) {
+                idCuenta = st.getInt("idSecretaria")
+                scndAct.putExtra("idCuenta", idCuenta)
+                startActivity(scndAct)
+                Toast.makeText(applicationContext, "Acceso completado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+            conx.dbConn()!!.close()
+        }
+    }
+
 }
