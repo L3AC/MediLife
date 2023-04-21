cfc69568f2618b5ececaef09b7f9fa622d208fa5
Gerson <20220175@ricaldone.edu.sv>
2023-04-21 14:39:25 -0600

    Cambio2

--- app/src/main/java/com/example/medilife/RecupContra.kt
+++ app/src/main/java/com/example/medilife/RecupContra.kt
@@ -1,11 +1,24 @@
 package com.example.medilife
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class RecupContra : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recup_contra)
+
+    val imageButton = findViewById<ImageButton>(R.id.imageButton4)
+        imageButton.setBackgroundColor(ContextCompat.getColor(this,android.R.color.transparent))
+
+        val textView = findViewById<TextView>(R.id.editTextTextPersonName)
+        textView.setBackgroundColor(ContextCompat.getColor(this,android.R.color.white))
     }
 }
