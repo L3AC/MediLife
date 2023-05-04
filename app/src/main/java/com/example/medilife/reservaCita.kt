f1915e42d7aad7ea46699411a0d58015b43e6dc8
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-05-03 21:08:24 -0600

    cambio

--- app/src/main/java/com/example/medilife/reservaCita.kt
+++ app/src/main/java/com/example/medilife/reservaCita.kt
@@ -10,25 +10,41 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.SQLException
 
-class espe(val id: Int, val nombre: String)
+class espeL(val id: Int, val nombre: String)
 
-val espec = mutableListOf<espe>()
+val especL = mutableListOf<espeL>()
 
 class doc(val id: Int, val nombre: String)
 
 val doctores = mutableListOf<doc>()
+lateinit var cbEsp:Spinner
+lateinit var cbDoc:Spinner
+lateinit var lbHorario:TextView
+
+private var spinDoc:String=""
+private var conx = Conx()
+private var nEsp:String=""
+private var idEsp:Int=0
+private var nDoctor:String=""
+private var idDoctor:Int=0
 
 class reservaCita : Fragment() {
-    private var conx = Conx()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            cbEsp=requireView().findViewById(R.id.spinEsp)
+            cbDoc=requireView().findViewById(R.id.spinDoc)
+            lbHorario=requireView().findViewById(R.id.txvHorario)
+            cbDoc.isEnabled=false
+            lbHorario.isVisible=false
 
         }
     }
@@ -45,13 +61,60 @@ class reservaCita : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
     }
-
-    fun SpinDoc(cb: Spinner) {
+    fun SpinEsp(cb: Spinner) {
         try {
-            val cadena = "select * from tbDoctores;"
+            val cadena = "select * from tbEspecialidades;"
             val st: ResultSet
             val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+            ps.setString(1,idEsp.toString())
 
+            st = ps.executeQuery()
+            //LLENAR SPINNER
+            while (st.next()) {
+                val idEsp = st.getString("idEspecialidad").toInt()
+                val especi = st.getString("especialidad")
+                especL.add(espeL(idEsp, "$especi"))
+            }
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,
+                especL.map { it.nombre })
+            cb.adapter = adapter
+            conx.dbConn()!!.close()
+            cbDoc.isEnabled=true //HABILITAR EL OTRO SPIN
+
+            cb.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    spinDoc = parent.getItemAtPosition(position).toString()
+                    val espc = especL[position]
+                    nEsp = espc.nombre
+                    idEsp = espc.id
+                    //LLENAR EL OTRO SPIN
+                    SpinDoc(cbDoc)
+
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+            }
+
+        } catch (ex: SQLException) {
+            Log.e("Error: ", ex.message!!)
+            Toast.makeText(context, "No existen especialidades", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun SpinDoc(cb: Spinner) {
+        try {
+            val cadena = "select idDoctor,CONCAT(nombres,' ',apellidos) as nombre " +
+                    "from tbDoctores where idEspecialidad=?;"
+            val st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+            ps.setString(1,idEsp.toString())
 
             st = ps.executeQuery()
             //LLENAR SPINNER
@@ -73,10 +136,11 @@ class reservaCita : Fragment() {
                     position: Int,
                     id: Long
                 ) {
-                    /*spinText = parent.getItemAtPosition(position).toString()
+                    spinDoc = parent.getItemAtPosition(position).toString()
                     val doct = doctores[position]
                     nDoctor = doct.nombre
-                    idDoctor = doct.id*/
+                    idDoctor = doct.id
+
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
