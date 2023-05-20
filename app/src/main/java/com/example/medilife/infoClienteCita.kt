f4d6225f0afb8c6e152c0095fe596de728113036
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-05-20 14:38:59 -0600

    cambio

--- app/src/main/java/com/example/medilife/infoClienteCita.kt
+++ app/src/main/java/com/example/medilife/infoClienteCita.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.isVisible
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.SQLException
@@ -19,12 +20,12 @@ lateinit var txtNombres: EditText
 lateinit var txtApellid2: EditText
 lateinit var txtNacimiento: EditText
 lateinit var txtSexo: EditText
-lateinit var spinSexoIC:Spinner
 lateinit var txtIdent: EditText
 lateinit var txtTipoSangre: EditText
 lateinit var txtTelf: EditText
 lateinit var txtPatolog: EditText
-lateinit var btnGuardar:Button
+lateinit var btnGuardar: Button
+lateinit var btnEditar: Button
 lateinit var btnVolver: Button
 lateinit var tpsexo2: Spinner
 
@@ -34,7 +35,7 @@ class infoClienteCita : Fragment() {
     var nivelC: Int = 0
     var idCliente: Int = 0
     private var conx = Conx()
-    val sexo = listOf("Femenino", "Masculino")
+   // val sexo = listOf("Femenino", "Masculino")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,20 +63,6 @@ class infoClienteCita : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        txtNombres = requireView().findViewById(R.id.txtNombres)
-        txtApellid2 = requireView().findViewById(R.id.txtApellid2)
-        txtNacimiento = requireView().findViewById(R.id.txtNacimiento)
-        txtSexo = requireView().findViewById(R.id.txtSexo)
-        spinSexoIC=requireView().findViewById(R.id.spinSexoIC)
-        txtIdent = requireView().findViewById(R.id.txtIdent)
-        txtTipoSangre = requireView().findViewById(R.id.txtTipoSangre)
-        txtTelf = requireView().findViewById(R.id.txtTelf)
-        txtPatolog = requireView().findViewById(R.id.txtPatolog)
-        btnGuardar = requireView().findViewById(R.id.btnGuardar)
-
-        LLenarSpin()
-        /*cbNombre.setSelectedItem(st.getString("Nombre"));
         txtNombres = requireView().findViewById(R.id.txtNombres)
         txtApellid2 = requireView().findViewById(R.id.txtApellid2)
         txtNacimiento = requireView().findViewById(R.id.txtNacimiento)
@@ -84,19 +71,43 @@ class infoClienteCita : Fragment() {
         txtTipoSangre = requireView().findViewById(R.id.txtTipoSangre)
         txtTelf = requireView().findViewById(R.id.txtTelf)
         txtPatolog = requireView().findViewById(R.id.txtPatolog)
-        btnGuardar = requireView().findViewById(R.id.btnGuardar)*/
+        btnEditar = requireView().findViewById(R.id.btnEditar2)
+        btnGuardar = requireView().findViewById(R.id.btnGuardar2)
+
+        //LLenarSpin()
+        txtNombres.isEnabled = false
+        txtApellid2.isEnabled = false
+        txtNacimiento.isEnabled = false
+        txtSexo.isEnabled = false
+        txtIdent.isEnabled = false
+        txtTipoSangre.isEnabled = false
+        txtTelf.isEnabled = false
+        txtPatolog.isEnabled = false
+        btnGuardar.isVisible = false
+
 
         CargarDatos()
-        btnGuardar.setOnClickListener(){
-
+        btnEditar.setOnClickListener() {
+            if (btnGuardar.isVisible) {
+                btnEditar.text = "Editar"
+                btnGuardar.isVisible = false
+                txtPatolog.isEnabled=false
+            } else {
+                btnEditar.text = "Cancelar"
+                btnGuardar.isVisible = true
+                txtPatolog.isEnabled=true
+            }
+        }
+        btnGuardar.setOnClickListener() {
+            updatePat()
         }
 
     }
+
     fun CargarDatos() {
         try {
-            val adapti=LLenarSpin()
-            var cadena: String =
-                "select * from tbClientes where idCliente=?;"
+           // val adapti = LLenarSpin()
+            var cadena = "select * from tbClientes where idCliente=?;"
             var st: ResultSet
             val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
 
@@ -108,15 +119,7 @@ class infoClienteCita : Fragment() {
             txtApellid2.setText(st.getString("apellidos"))
             txtNacimiento.setText(st.getString("nacimiento"))
             txtSexo.setText(st.getString("sexo"))
-
-            /*val selection=st.getString("sexo")
-            if(st.getString("sexo")=="Masculino"){
-
-            }
-            else{
-
-            }*/
-            spinSexoIC.setSelection(adapti.getPosition(st.getString("sexo")));
+            //spinSexoIC.setSelection(adapti.getPosition(st.getString("sexo")));
             txtIdent.setText(st.getString("numdocum"))
             txtTipoSangre.setText(st.getString("tipoSangre"))
             txtTelf.setText(st.getString("telefono"))
@@ -127,43 +130,34 @@ class infoClienteCita : Fragment() {
             Toast.makeText(context, "Error al mostrar", Toast.LENGTH_SHORT).show()
         }
     }
-   /* fun updatePat() {
 
+    fun updatePat() {
         try {
             val cadena: String =
                 "UPDATE tbClientes SET patologias =? WHERE idCliente=?;"
 
             val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
-
-            /*ps.setString(1, idus.toString())
-            ps.setString(2, nomb.text.toString())
-            ps.setString(3, apell.text.toString())
-            ps.setString(4, tpdoc.selectedItem.toString())
-            ps.setString(5, ndoc.text.toString())
-            ps.setString(6, fechaSql)
-            ps.setString(7, tpsexo.selectedItem.toString())
-            ps.setString(8, tel.text.toString())
-            ps.setString(9, tpsangre.text.toString())
-            ps.setString(10, patol.text.toString())*/
+            ps.setString(1, txtPatolog.text.toString())
+            ps.setInt(2, idCliente)
 
             ps.executeUpdate()
-
+            Toast.makeText(context, "Campo actualizado", Toast.LENGTH_SHORT).show()
         } catch (ex: SQLException) {
             Log.e("Error: ", ex.message!!)
-            Toast.makeText(applicationContext, "Errorsito", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Errorsito", Toast.LENGTH_SHORT).show()
         }
         conx.dbConn()!!.close()
 
+    }
+
+    /*fun LLenarSpin(): ArrayAdapter<String> {
+        val adaptadorSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sexo)
+        adaptadorSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = requireView().findViewById<Spinner>(R.id.spinSexoIC)
+        spinner.adapter = adaptadorSpinner
+        return adaptadorSpinner
+
     }*/
-
-   fun LLenarSpin(): ArrayAdapter<String> {
-       val adaptadorSpinner = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sexo)
-       adaptadorSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-       val spinner = requireView().findViewById<Spinner>(R.id.spinSexoIC)
-       spinner.adapter = adaptadorSpinner
-
-       return adaptadorSpinner
-
-   }
 
 }
--- app/src/main/java/com/example/medilife/infoCuentaGeneral.kt
+++ app/src/main/java/com/example/medilife/infoCuentaGeneral.kt
@@ -9,23 +9,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import java.util.Date
 
-lateinit var Usuario:EditText
-lateinit var Apellido:EditText
-lateinit var Doc:EditText
-lateinit var Telefono:EditText
-
+lateinit var txtUsuario3:EditText
+lateinit var txtNombres3: EditText
+lateinit var txtApellid3: EditText
+lateinit var txtNacimiento3: EditText
+lateinit var txtSexo3: EditText
+lateinit var spinSexoIC: Spinner
+lateinit var txtIdent3: EditText
+lateinit var txtTipoSangre3: EditText
+lateinit var txtTelf3: EditText
+lateinit var txtPatolog3: EditText
+lateinit var btnGuardar3: Button
+lateinit var btnEditar3: Button
 
 class infoCuentaGeneral : Fragment() {
     var idCuenta: Int =0
-
+    val sexo = listOf("Femenino", "Masculino")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -44,6 +54,29 @@ class infoCuentaGeneral : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        txtNombres3 = requireView().findViewById(R.id.txtNombres)
+        txtApellid3 = requireView().findViewById(R.id.txtApellid2)
+        txtNacimiento3 = requireView().findViewById(R.id.txtNacimiento)
+        txtSexo3 = requireView().findViewById(R.id.txtSexo)
+        txtIdent3 = requireView().findViewById(R.id.txtIdent)
+        txtTipoSangre3 = requireView().findViewById(R.id.txtTipoSangre)
+        txtTelf3 = requireView().findViewById(R.id.txtTelf)
+        txtPatolog3 = requireView().findViewById(R.id.txtPatolog)
+        btnEditar3 = requireView().findViewById(R.id.btnEditar)
+        btnGuardar3 = requireView().findViewById(R.id.btnGuardar)
+
+        LLenarSpin()
+        txtNombres3.isEnabled = false
+        txtApellid3.isEnabled = false
+        txtNacimiento3.isEnabled = false
+        txtSexo3.isEnabled = false
+        txtIdent3.isEnabled = false
+        txtTipoSangre3.isEnabled = false
+        txtTelf3.isEnabled = false
+        txtPatolog3.isEnabled = false
+        btnGuardar3.isVisible = false
+
         val bundle = Bundle().apply {
             putInt("idcu", idCuenta)
         }
@@ -56,7 +89,13 @@ class infoCuentaGeneral : Fragment() {
         }
 
     }
+    fun LLenarSpin(): ArrayAdapter<String> {
+        val adaptadorSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sexo)
+        adaptadorSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = requireView().findViewById<Spinner>(R.id.spinSexo3)
+        spinner.adapter = adaptadorSpinner
+        return adaptadorSpinner
 
-
-
+    }
 }
